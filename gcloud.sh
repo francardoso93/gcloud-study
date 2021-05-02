e5f9494d0ad1fae8b2268c5f897592da03e448e8
Francisco Cardoso <francardoso@outlook.com>
2021-05-02 16:48:05 -0300

    scp

--- gcloud.sh
+++ gcloud.sh
@@ -7,6 +7,8 @@ gcloud compute networks subnets create clustersubnet --network clusternet --rang
 gcloud compute instances create main --zone=us-east4-c --machine-type=n2-standard-2 --image-project ubuntu-os-cloud --image-family=ubuntu-minimal-2004-lts --boot-disk-size=20GB --network=clusternet --subnet=clustersubnet 
 gcloud compute instances create worker --zone=us-east4-c --machine-type=n2-standard-2 --image-project ubuntu-os-cloud --image-family=ubuntu-minimal-2004-lts --boot-disk-size=20GB --network=clusternet --subnet=clustersubnet 
 # Esse comando faz o acesso SSH pra mim! (Antes ele seta a chave ssh como confiável) # https://stackoverflow.com/questions/27535945/how-to-get-the-ssh-keys-for-a-new-google-compute-engine-instance
-gcloud compute ssh main --ssh-key-file=.ssh/id_rsa  
-gcloud compute ssh worker --ssh-key-file=.ssh/id_rsa  
+gcloud compute ssh main --ssh-key-file=~/.ssh/id_rsa  
+gcloud compute ssh worker --ssh-key-file=~/.ssh/id_rsa  
+gcloud compute scp $(find $HOME -name k8sMaster.sh) main:~ --ssh-key-file=~/.ssh/id_rsa
 # E entrei! =) I'm in bitches!
+
